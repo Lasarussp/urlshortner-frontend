a725824a0592390c02143c01c2dff48d246da55f
Lasarussp <001lasarus@gmail.com>
2023-07-12 17:19:22 +0530

    url updated

--- src/Global.js
+++ src/Global.js
@@ -1,2 +1,3 @@
- export const API = "http://localhost:5000";
-//export const API = "";
+//export const API = "http://localhost:5000";
+//export const API = "https://url-shortner-backend-ky7v.onrender.com";
+export const API = "https://urlshortner-backend-weld.vercel.app";
